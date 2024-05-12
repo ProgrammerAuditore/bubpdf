1fd42e3b2fa8ca2f419edcc3b9184dd92fb5ff71
ProgrammerAuditore <victorvj098@gmail.com>
2024-05-11 19:39:24 -0600

    *** feature
    !!! feature
    
    - Se modifico la configuración para DataTableJS

--- resources/views/paginas/empleados.blade.php
+++ resources/views/paginas/empleados.blade.php
@@ -8,7 +8,7 @@
 
 @section('contenido-principal')
 <div class="datatable-css">
-    <table id="example" class="table table-striped table-bordered">
+    <table id="crudTable" ref="crudTable" class="table table-striped table-bordered">
         <thead class="table-warning">
             <tr>
                 <th>Name</th>
@@ -53,6 +53,241 @@
                 <td>$112,000</td>
             </tr>
         </tbody>
+        <tfoot>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </tfoot>
     </table>
 </div>
 @endsection
+
+@push('scripts')
+<script>
+    $(document).ready(function() {
+        var crudTable = $('#crudTable').DataTable({
+            id: 'crudTable',
+            layout: {
+                topStart: {
+                    buttons: [{
+                        text: 'My button',
+                        action: function(e, dt, node, config) {
+                            alert('Button activated');
+                        }
+                    }]
+                }
+            },
+            // !! Cambiar el idioma al español
+            language: {
+                url: '{{ asset("json/datatables.spanish.json") }}',
+            },
+            searching: true,
+            autoWidth: true,
+            info: true,
+            paging: true,
+            pagingType: "full_numbers",
+            lengthMenu: [
+                [10, 25, 50, 100, 500, 1000, -1],
+                [10, 25, 50, 100, 500, 1000, "Todos"]
+            ],
+            pageLength: 10,
+            columnDefs: [{
+                    className: "d-text text-center",
+                    targets: "_all"
+                },
+                {
+                    style: {
+                        "white-space": "normal"
+                    },
+                    targets: "_all"
+                },
+            ],
+            // !! >>> Crear la tabla responsiva
+            responsive: false,
+            /*
+            fixedHeader: true,
+            responsive: {
+                details: {
+                    display: $.fn.dataTable.Responsive.display.modal( {
+                        header: function ( row ) {
+                            // show the content of the first column
+                            // as the modal header
+                            // var data = row.data();
+                            // return data[0];
+                            return '';
+                        }
+                    } ),
+                    renderer: function ( api, rowIdx, columns ) {
+
+                    var data = $.map( columns, function ( col, i ) {
+                        var columnHeading = crudTable.columns().header()[col.columnIndex];
+
+                        // hide columns that have VisibleInModal false
+                        if ($(columnHeading).attr('data-visible-in-modal') == 'false') {
+                            return '';
+                        }
+
+                        return '<tr data-dt-row="'+col.rowIndex+'" data-dt-column="'+col.columnIndex+'">'+
+                                    '<td style="vertical-align:top; border:none;"><strong>'+col.title.trim()+':'+'<strong></td> '+
+                                    '<td style="padding-left:10px;padding-bottom:10px; border:none;">'+col.data+'</td>'+
+                                '</tr>';
+                    } ).join('');
+
+                    return data ?
+                        $('<table class="table table-striped mb-0">').append( '<tbody>' + data + '</tbody>' ) :
+                        false;
+                    },
+                }
+            },
+            */
+            // !! >>> Exportación de datos  <<<<
+            buttons: {
+                buttons: [{
+                        extend: "collection",
+                        text: '<i class="bi bi-download"></i>&nbsp;&nbsp;Exportar',
+                        fade: false,
+                        buttons: [{
+                                extend: "csv",
+                                title: "Lista de empleados",
+                                exportOptions: {
+                                    modifier: {
+                                        page: "all",
+                                    },
+                                },
+                            },
+                            {
+                                extend: "excel",
+                                title: "Lista de empleados",
+                                exportOptions: {
+                                    modifier: {
+                                        page: "all",
+                                    },
+                                },
+                            },
+                            {
+                                extend: "pdfHtml5",
+                                title: "Lista de empleados",
+                                exportOptions: {
+                                    modifier: {
+                                        page: "all",
+                                    },
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        extend: "copy",
+                        className: "btn btn-primary glyphicon glyphicon-duplicate",
+                    },
+                    // !! >>> Crear botones personalizados
+                    /*
+                    {
+                        text: 'Select all',
+                        className: 'btn btn-sm btn-outline-blue',
+                        action: function() {
+                            reassignQueryTable.rows().select();
+                        }
+                    },
+                    {
+                        text: 'Select none',
+                        className: 'btn btn-sm btn-outline-red',
+                        action: function() {
+                            reassignQueryTable.rows().deselect();
+                        }
+                    },
+                    {
+                        extend: 'collection',
+                        text: 'Table control',
+                        buttons: [{
+                                text: 'Toggle start date',
+                                action: function(e, dt, node, config) {
+                                    dt.column(-2).visible(!dt.column(-2).visible());
+                                }
+                            },
+                            {
+                                text: 'Toggle salary',
+                                action: function(e, dt, node, config) {
+                                    dt.column(-1).visible(!dt.column(-1).visible());
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        text: 'Crear usuario',
+                        action: function(e, dt, node, config) {
+                            alert('Button activated');
+                        }
+                    },
+                    */
+                ],
+                dom: {
+                    button: {
+                        tag: "button",
+                        className: "btn btn-sm btn-secondary",
+                    },
+                    buttonLiner: {
+                        tag: "null",
+                    },
+                },
+            },
+            // !! Crear campos de busqueda
+            initComplete: function() {
+                this.api()
+                    .columns()
+                    .every(function(index) {
+                        let column = this;
+                        const skip = column.footer().getAttribute('data-skip');
+
+                        if (skip === 'on' || skip === 'true') {
+                            return;
+                        }
+
+                        // get title
+                        let title = column.footer().textContent;
+
+                        // Create input element
+                        let input = document.createElement('input');
+                        input.style.width = '100%';
+                        input.placeholder = title;
+                        input.classList.add('form-control');
+                        column.footer().replaceChildren(input);
+
+                        // Event listener for user input
+                        input.addEventListener('keyup', () => {
+                            if (column.search() !== this.value) {
+                                column.search(input.value).draw();
+                            }
+                        });
+                    });
+            },
+            // !! >>> Configure Ajax <<<<
+            /*
+            processing: true,
+            serverSide: true,
+            ajax: {
+                "url": "https://",
+                "type": "POST"
+            },
+            */
+            // !! >>> Distribución de información  <<<< // <'col-sm-12 col-md-4'B>
+            //dom: "Bfrtip",
+            dom: "<'row hidden'<'col-sm-6'i><'col-sm-6 d-print-none'f>>" +
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row mt-2 d-print-none '<'col-sm-12 col-md-8'l><'col-sm-12 col-md-4 'p>>B",
+        });
+    });
+</script>
+@endpush
--- resources/views/plantilla/base.blade.php
+++ resources/views/plantilla/base.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
@@ -11,8 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
     <!-- DataTables CSS -->
@@ -22,7 +22,7 @@
     <title>
         BubPDF
         @isset($__env->getSections()['pagina-titulo'])
-            | @yield('pagina-titulo')
+        | @yield('pagina-titulo')
         @endif
     </title>
 
@@ -33,6 +33,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
 </head>
+
 <body class="d-flex flex-column min-vh-100">
     @include('componentes.main_header')
 
@@ -47,11 +48,17 @@
     @include('componentes.pie_pagina')
 
     <!-- JavaScript Files -->
-    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
     <script src="{{ asset('assets/js/datatables.min.js') }}"></script>
     <script src="{{ asset('assets/js/pdfmake.min.js') }}"></script>
     <script src="{{ asset('assets/js/vfs_fonts.js') }}"></script>
-    <script src="{{ asset('assets/js/custom.js') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+    <!-- Bloque para scripts adicionales -->
+    @stack('scripts')
+
 </body>
+
 </html>
